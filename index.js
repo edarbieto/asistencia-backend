5ee22a9286802a22873dd1b722f4caded6779933
edarbieto <edarbieto@gmail.com>
2022-10-07 11:55:53 -0500

    enable cors for production

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:
+      process.env.ENVIRONMENT === "PROD"
+        ? "http://localhost"
+        : "http://localhost:5173",
   })
 );
 
